dc09e24b3e6b97116b8b2ebc18aa6fdedc80f692
victor <victorvilascv@gmail.com>
2023-10-01 17:23:30 +0200

    Se agregó una línea

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        System.out.println("Esta instrucción no sirve");
         return "%s %s".formatted(nombre,apellidos);
     }
 }
