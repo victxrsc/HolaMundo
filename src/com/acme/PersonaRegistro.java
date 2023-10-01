3c3d63052fe28cd6b3d40363ab5614f89a7195d8
victor <victorvilascv@gmail.com>
2023-10-01 17:22:23 +0200

    Se creó el Registro Persona

---
+++ src/com/acme/PersonaRegistro.java
@@ -0,0 +1,8 @@
+package com.acme;
+
+public record PersonaRegistro(String nombre, String apellidos) {
+    @Override
+    public String toString() {
+        return "%s %s".formatted(nombre,apellidos);
+    }
+}
