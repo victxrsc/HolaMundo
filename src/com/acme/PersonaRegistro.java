69b143badc2b11331b595fa957fce96e6be64fce
victxrsc <95866391+victxrsc@users.noreply.github.com>
2023-10-01 17:32:10 +0200

    Comentario

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        //Comentario para eliminar línea
         System.out.println("Esta instrucción no sirve");
         return "%s %s".formatted(nombre,apellidos);
     }
